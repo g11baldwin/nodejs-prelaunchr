51255310f4113e9b33cf2cb3b3ca4462c5d28a79
Gary Baldwin <g11baldwin@gmail.com>
2015-12-11 11:36:38 -0500

    start of changes for debugging duplicate ip checking
    Signed-off-by: Gary Baldwin <g11baldwin@gmail.com>

--- config/http.js
+++ config/http.js
@@ -54,10 +54,10 @@ module.exports.http = {
   *                                                                           *
   ****************************************************************************/
 
-    // myRequestLogger: function (req, res, next) {
-    //     console.log("Requested :: ", req.method, req.url);
-    //     return next();
-    // }
+     myRequestLogger: function (req, res, next) {
+         console.log("Requested :: ", req.method, req.url);
+         return next();
+     },
 
 
   /***************************************************************************
